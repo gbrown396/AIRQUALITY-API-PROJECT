9ccf4e1b548586a84ba18eaa4c25f5e3d423a930
George <george@Georges-MacBook-Pro.local>
2018-10-11 01:47:48 -0400

    second commit

--- AQ.js
+++ AQ.js
@@ -1,7 +1,20 @@
 const baseURL = 'https://api.openaq.org/v1/locations';
 let url;
 
-const searchTerm = document.querySelector('.search');
+function searchToggle(obj, evt){
+    var container = $(obj).closest('.search-wrapper');
+        if(!container.hasClass('active')){
+            container.addClass('active');
+            evt.preventDefault();
+        }
+        else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+            container.removeClass('active');
+            // clear input
+            container.find('.search-input').val('');
+        }
+}
+
+const searchTerm = document.querySelector('.search-input');
 const searchForm = document.querySelector('form');
 const section = document.querySelector('section');
 
@@ -12,7 +25,7 @@ function fetchResults(e) {
     url =`${baseURL}/?location=${searchTerm.value}`; 
     console.log('URL:',url);
 
-    fetch(baseURL)
+    fetch(url)
     .then(function(result){
     //  console.log(result)
      return result.json();   
@@ -28,10 +41,30 @@ function displayResults(json){
         section.removeChild(section.firstChild);
     } console.log(json);
     console.log(json.results["0"].location);
-    console.log(json.results["0"].city)
+    console.log(json.results["0"].country)
     console.log(json.results["0"].count)
     console.log(json.results["0"].parameters)
-} 
+
+let para1 = document.createElement('p');
+       para1.setAttribute("class", "loction");
+   let para2 = document.createElement('p');
+       para2.setAttribute("class", "country");
+   let para3 = document.createElement('p');
+       para3.setAttribute("class", "count");    
+    let para4 = document.createElement('p');
+       para4.setAttribute("class", "parameters");
+
+   para1.innerText = json.results["0"].location;
+   para2.innerText = json.results["0"].country;
+   para3.innerText = json.results["0"].count;
+   para4.innerText = json.results["0"].parameters;
+
+
+   section.appendChild(para1);
+   section.appendChild(para2);
+   section.appendChild(para3);
+   section.appendChild(para4);
+}
     // section.appendChild();
 
 
